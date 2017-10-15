756a351ae76414ac8d4a19331e9b09f27d81b13e
Xiangru Shu <sophiashu@Xiangrus-MacBook-Air.local>
2017-10-15 14:25:48 -0400

    tested 3 coefficient functions and progress_rate

---
+++ test_chemkin.py
@@ -0,0 +1,60 @@
+import numpy as np
+import chemkin
+
+#test k_const
+def test_k_const():
+    assert chemkin.ChemUtil.k_const(4.0) == 4.0
+    
+def test_k_const_neg():
+    try:
+        chemkin.ChemUtil.k_const(-1.0)
+    except ValueError as err:
+        assert(type(err) == ValueError)
+
+#test k_arr
+def test_k_arr():
+    assert chemkin.ChemUtil.k_arr(2.0, 3.0, 100.0) == 1.9927962618542914
+    
+def test_k_arr_A_neg():
+    try:
+        chemkin.ChemUtil.k_arr(-1.0, 3.0, 100.0)
+    except ValueError as err:
+        assert(type(err) == ValueError)
+        
+def test_k_arr_T_neg():
+    try:
+        chemkin.ChemUtil.k_arr(1.0, 3.0, -100.0)
+    except ValueError as err:
+        assert(type(err) == ValueError)
+
+def test_k_arr_R_neg():
+    try:
+        chemkin.ChemUtil.k_arr(1.0, 3.0, 100.0, -45.0)
+    except ValueError as err:
+        assert(type(err) == ValueError)
+
+#test k_mod_arr
+def test_k_mod_arr():
+    assert chemkin.ChemUtil.k_mod_arr(2.0, -0.5, 3.0, 100.0) == 0.19927962618542916
+    
+def test_k_mod_arr_A_neg():
+    try:
+        chemkin.ChemUtil.k_mod_arr(-1.0, 3.0, 4.0, 100.0)
+    except ValueError as err:
+        assert(type(err) == ValueError)
+        
+def test_k_mod_arr_T_neg():
+    try:
+        chemkin.ChemUtil.k_mod_arr(1.0, 3.0, 4.0, -100.0)
+    except ValueError as err:
+        assert(type(err) == ValueError)
+
+def test_k_mod_arr_R_neg():
+    try:
+        chemkin.ChemUtil.k_mod_arr(1.0, 3.0, 100.0, -4.9)
+    except ValueError as err:
+        assert(type(err) == ValueError)
+
+#test progress_rate
+def test_progress_rate():
+    assert chemkin.ChemUtil.progress_rate(np.array([[2.0, 1.0], [1.0, 0.0], [0.0, 1.0]]), np.array([2.0, 1.0, 1.0]), 10.0) == array([ 40.,  20.])
