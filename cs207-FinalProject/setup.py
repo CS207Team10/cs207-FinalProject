0d185b8c65b5c21b54e5f89604cf6def08cc3f25
hidetana18 <31484044+hidetana18@users.noreply.github.com>
2017-11-19 14:48:11 -0500

    updates

--- cs207-FinalProject/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='cs207-FinalProject',
-      version='0.1',
-      description='chemical kinetics library',
-      url='https://github.com/CS207Team10/cs207-FinalProject',
-      author='Hidenori Tanaka, Jiachen Song, Xiangru Shu',
-      packages=['cs207-FinalProject'],
-      zip_safe=False)
