9cda3d286c6e69e4249c2e026806e630e8735386
Jason Song <jiachens@andrew.cmu.edu>
2017-11-20 11:02:44 -0500

    add eqn images

--- samples/irreversible.py
+++ samples/irreversible.py
@@ -6,8 +6,8 @@ R = 8.314
 concs = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
 rsystem = ck.ReactionSystem(T, R, "../tests/data/db/nasa.sqlite")
 rsystem.buildFromXml("../tests/data/xml/rxnset_long.xml", concs)
-print("Progress rate: \n", rsystem.getProgressRate())
-print("Reaction rate: \n", rsystem.getReactionRate())
-# print(rsystem)
+print("Progress rate: \n", rsystem.getProgressRate(), "\n")
+print("Reaction rate: \n", rsystem.getReactionRate(), "\n")
+# print("System info: \b", rsystem, "\n")
 
     
