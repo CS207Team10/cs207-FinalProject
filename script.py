274455c89ca546953673592f5cdfbcbc95c2dfcf
shuxiangru <31483841+shuxiangru@users.noreply.github.com>
2017-11-19 23:29:24 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-import package.chemkin_2 as ch
+from chemkin_G10 import chemkin
 import numpy as np
 
 T_DEFAULT = 1500
@@ -6,7 +6,7 @@ R_DEFAULT = 8.314
 
 if __name__ == '__main__':
     concs = np.array([2.0, 1.0, 0.5, 1.0, 1.0])
-    rsystem = ch.ReactionSystem(T_DEFAULT, R_DEFAULT, concs)
+    rsystem = chemkin.ReactionSystem(T_DEFAULT, R_DEFAULT, concs)
     rsystem.buildFromXml("input files/rxns_reversible.xml")
 
     print(rsystem.getProgressRate())
