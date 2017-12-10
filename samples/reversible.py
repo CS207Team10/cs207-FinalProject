f2a731b365e2cc8724c7cf246d11f3fa2883a7f5
Jason Song <jiachens@andrew.cmu.edu>
2017-12-10 09:41:57 -0500

    fix bug - db max min order reversed

--- samples/reversible.py
+++ samples/reversible.py
@@ -1,13 +1,13 @@
 from chemkin_g10 import chemkin as ck
 import numpy as np
 
-T = 1500
+T = 2500.0
 R = 8.314
-concs = np.array([1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+concs = np.array([0.5, 0, 0, 2, 0, 1, 0, 0])
 rsystem = ck.ReactionSystem(T, R, "../tests/data/db/nasa.sqlite")
 rsystem.buildFromXml("../tests/data/xml/rxns_reversible.xml", concs)
-print("Progress rate: \n", rsystem.getProgressRate(), "\n")
+# print("Progress rate: \n", rsystem.getProgressRate(), "\n")
 print("Reaction rate: \n", rsystem.getReactionRate(), "\n")
-# print("System info: \b", rsystem, "\n")
+# print("System info: \n", rsystem, "\n")
 
     
